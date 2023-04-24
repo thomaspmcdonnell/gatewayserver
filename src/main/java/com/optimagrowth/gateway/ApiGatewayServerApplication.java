3f6d6a112d28b337a4dbb5b2a3b6546cd58d5d25
McDonnell, Thomas Paul <tmcdonne@iu.edu>
2023-04-24 18:00:28 -0400

    Initial commit

---
+++ src/main/java/com/optimagrowth/gateway/ApiGatewayServerApplication.java
@@ -0,0 +1,20 @@
+package com.optimagrowth.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
